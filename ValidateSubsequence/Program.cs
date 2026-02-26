f9a7fb590575fbe08ef22090e0a613ff0d6d61ea
NtshetsoPele <mothobi.bald@gmail.com>
2026-02-26 17:39:51 +0200

    Valid Subsequence implementation

--- ValidateSubsequence/Program.cs
+++ ValidateSubsequence/Program.cs
@@ -1,30 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
+﻿
+// indices                  0, 1, 2,  3,  4, 5,  6, 7
 var array = new List<int> { 5, 1, 22, 25, 6, -1, 8, 10 };
 var sequence = new List<int> { 1, 6, -1, 10 };
 
-Console.WriteLine(IsValidSubsequence(array, sequence));
-
-return;
-
-static bool IsValidSubsequence(List<int> array, List<int> sequence)
-{
-    // Write your code here.
-    var q1 = new Queue<int>(array);
-    var q2 = new Queue<int>(sequence);
-    while (q2.Count > 0)
-    {
-        if (q1.Peek() == q2.Peek())
-        {
-            q1.Dequeue();
-            q2.Dequeue();
-        }
-        else
-        {
-            q1.Dequeue();
-        }
-    }
-
-    return q2.Count == 0;
-}
+Console.WriteLine(Subsequence.IsValidSubsequence(array, sequence));
---
+++ ValidateSubsequence/Subsequence.cs
@@ -0,0 +1,24 @@
+﻿namespace ValidateSubsequence;
+
+public static class Subsequence
+{
+    public static bool IsValidSubsequence(List<int> array, List<int> sequence)
+    {
+        var sequenceIndex = 0;
+
+        return sequence.All((int nextNum) => NextInRemainingSubarray(nextNum, ref sequenceIndex, array));
+    }
+
+    public static bool NextInRemainingSubarray(int nextNum, ref int sequenceIndex, List<int> array)
+    {
+        for (; sequenceIndex < array.Count;)
+        {
+            if (nextNum == array[sequenceIndex++])
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
